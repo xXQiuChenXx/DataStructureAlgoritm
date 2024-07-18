93aad6e5074302b29038cff73e9ae15cd404ca44
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-18 19:31:50 +0800

    Exercise 6

---
+++ Lab1/Exercise6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int m, n, sum = 0;
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if(m > n) {
+        printf("Invalid range: m value cannot be greater than n value\n");
+        return 0;
+    }
+
+    for(int i = m  ; i <= n; i++) {
+        sum += i;
+    }
+    printf("The sum of numbers from %d to %d is %d \n", m, n, sum);
+    return 0;
+}
