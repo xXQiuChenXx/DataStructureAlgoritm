4391026e7c94eb4f38ab3d4407919d8629fb42ff
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-18 19:07:04 +0800

    rename and Exercise 3

---
+++ Lab1/Exercise3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    float f_num = (float)number;
+    printf("The floating point variant of %d is %f \n ", number, f_num);
+    return 0;
+}
