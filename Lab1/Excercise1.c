b3ae1f48321c6fc16d5690203fa3338e41131381
xXQiuChenXx <lautaihong2004@gmail.com>
2024-07-18 14:27:06 +0800

    Excerciese1

---
+++ Lab1/Excercise1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    char letter;
+    float decimal = 7.5;
+    double double_num = 5.5;
+    num1 = 100;
+    num2 = 200;
+    letter = 'A';
+
+    printf("Num1 is %d \n", num1);
+    printf("Num2 is %d \n", num2);
+    printf("Letter is %c \n", letter);
+    printf("Decimal is %f \n", decimal);
+    printf("Double Number is %f \n", double_num);
+
+    return 0;
+}
